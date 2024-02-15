40aaaf8acfb9ec15ddc28a0c1d42cc4910bbb833
cereia <104402370+cereia@users.noreply.github.com>
2024-02-15 16:40:42 -0500

    Added and deleted comments for clarity

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-//create a function to determine the computer's choice
-//create a function that compares the computer and player's choices
-//create a function that determines the overall winner after 5 rounds
-
-// let computer;
-// let player;
 let playerPoints = 0;
 let computerPoints = 0;
 
 let getPlayerChoice = () => {
-  // return player = prompt('Rock, paper, or scissors?')
   return prompt('Rock, paper, or scissors?');
 };
 
+//get a random option from an array of 3 options
 function getComputerChoice() {
   let options = ['rock', 'paper', 'scissors'];
   let random = Math.floor(Math.random() * 3);
-  // return computer = options[random];
   return options[random];
 }
 
 function playRound(playerSelection, computerSelection) {
+  //get the choices and make them case insensitive 
   let player = playerSelection().toLowerCase();
-  let computer = computerSelection().toLowerCase();
-  let capP = player[0].toUpperCase() + player.slice(1); //capitalized player
-  let capC = computer[0].toUpperCase() + computer.slice(1); //capitalized computer
-  
-  let message;
+  let computer = computerSelection();
+
+  //capitalize the first letter of the choices
+  let capP = player[0].toUpperCase() + player.slice(1); 
+  let capC = computer[0].toUpperCase() + computer.slice(1); 
+
+  let message; 
+
+  //rock > scissors; scissors > paper; paper > rock
   if ( (player == 'rock' && computer == 'scissors') 
     || (player == 'scissors' && computer == 'paper') 
     || (player == 'paper' && computer == 'rock')
@@ -40,22 +38,24 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-//play game function has to use the previous function playRound to play 5 rounds
-//after 5 rounds, throw a message saying win, lose, or tie
-
+//playGame uses playRound to play 5 rounds
+//after 5 rounds, playGame compares the points logs a message saying win, lose, or tie
 function playGame() {
   let round = 0;
   for (let i = 0; i < 5; i++) {
     round += 1; 
     console.log('Round: ', round, playRound(getPlayerChoice, getComputerChoice));
   }
-  if(playerPoints > computerPoints) {
+  
+  if (playerPoints > computerPoints) {
     console.log(`You win! You won ${playerPoints} / 5 games!`);
   } else if (playerPoints < computerPoints) {
     console.log(`You lose! The computer won ${computerPoints} / 5 games!`);
   } else {
     console.log(`It's a tie! You both won ${playerPoints} / 5 games!`);
   }
+
+  //reset the scores for a new game
   playerPoints = 0;
   computerPoints = 0;
 }
